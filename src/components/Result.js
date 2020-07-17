2c83cbfa9f7e4101dd8ec77c03a0da678f2017d5
lewisjackson398 <lewisjackson398@gmail.com>
2020-07-17 23:54:10 +0100

    quiz

---
+++ src/components/Result.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
+
+function Result(props) {
+    return (
+        <CSSTransition
+            className="container result fade-appear-done"
+            component="div"
+            timeout={200}
+            appearDone
+        >
+            <div>
+                You prefer <strong>{props.quizResult}</strong>!
+      </div>
+        </CSSTransition>
+    );
+}
+
+Result.propTypes = {
+    quizResult: PropTypes.string.isRequired
+};
+
+export default Result;
